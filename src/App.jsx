0ea5123c7c1622213e0f96b77206f9d5c22cf7df
Satendra Sharma <sharmasatendra99@gmail.com>
2024-05-19 21:06:20 +0530

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css"
+import Navigation from "./components/navigation"
+import HeroSection from "./components/herosection"
+const App = () => {
+    return (
+        <div>
+           <Navigation/>
+           <HeroSection/>
+        </div>
+    )
+}
+
+export default App
---
+++ src/components/herosection.jsx
@@ -0,0 +1,26 @@
+const HeroSection = ()=> {
+    return (
+        <main className="hero">
+            <div>
+                <h1>YOUR FEET DESERVES THE BEST</h1>
+                <p>
+                   Your feet deserves the best and we're here to help you finding your best shoes at minimal and affordable rates. Happy shopping! 
+                </p>
+                <div className="buttonss">
+                    <button className="shop now">SHOP NOW</button>
+                    <button className="category">Category</button>
+                </div>
+                <div className="p tag">Happy Shopping!!!!!!</div>
+                <div className="extra images">
+                    <img src="images/flipkart.png" alt="flipkart-logo"/>
+                    <img src="images/amazon.png" alt="amazon-logo"/>
+                </div>
+                <div className="shoe img">
+                    <img src="images/shoe_image.png" alt="shoe-image"/>
+                </div> 
+            </div>
+        </main>
+    )
+}
+
+export default HeroSection;
